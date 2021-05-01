bfc71e6498f7f96a18ecd292382613fd9f0b0421
Jhoandry Sequera <jsequera826@gmail.com>
2021-05-01 20:17:00 -0300

    apolo studio connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import express from "express"
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log("hola scv")
-});
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
